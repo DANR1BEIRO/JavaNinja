943af0ca4d5e34339f33ab2a84921657a6e064bb
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-10 12:04:22 -0300

    Tipos primitivos

--- JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
+++ JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
@@ -3,5 +3,10 @@ package TiposDeDados;
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
 
+        /*
+        String, Array, Class, enum
+         */
+        String nome = "Naruto Uzumaki";
+        System.out.println(nome.toUpperCase());
     }
 }

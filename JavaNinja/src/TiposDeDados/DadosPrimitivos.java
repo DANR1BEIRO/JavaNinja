b5266789e828cdc06a38f8dfc7ec097468126a13
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-10 03:04:53 -0300

    Tipos primitivos

---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,26 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*Código nosso que está em Java
+Santificado seja vós, JVM
+Venha a nós o vosso [array]
+E seja feita, sim, vossa {chave}
+Assim no if como else
+O for nosso de cada dia nos dai hoje
+Debugai as nossas sentenças
+Assim como nós colocamos o ponto e vírgula (;) esquecidos
+ㅤㅤE não nos deixes errar a indentação
+Mas livrai-nos das funções recursivas
+A main()*/
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L;
+
+        System.out.println(idade);
+    }
+}

dabd778d055b7fdab5206058229410574c35c682
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-10 12:04:42 -0300

    If else statement

---
+++ JavaNinja/src/Conditions/IfElse.java
@@ -0,0 +1,21 @@
+package Conditions;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        String name = "Naruto Uzumaki";
+        int age = 16;
+        boolean isHokage = false;
+        short completedMissions = 19;
+
+        // if (condition is true) {do this}
+        if (completedMissions > 10 && age > 15) {
+            System.out.println(name + " is eligible for the Chunin exams based\non the number" +
+                    " of missions he has completed!");
+        } else {
+            System.out.println(name + " isn't eligible to rank up from Genin to Chunin\n" +
+                    " because he hasn't completed the minimum number of missions");
+        }
+
+    }
+}

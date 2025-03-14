cce39d356b254b63697766e7ad0640136b2a45ac
DANR1BEIRO <danribeiro001@gmail.com>
2025-03-14 10:47:44 -0300

    Ninja registration

---
+++ JavaNinja/src/Conditions/challenge02/NinjaRegistration.java
@@ -0,0 +1,65 @@
+package Conditions.challenge02;
+import Conditions.SwitchCases;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NinjaRegistration {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] ninjas = new String[5];
+        int ninjasRegistered = 0;
+
+        while(true) {
+            System.out.print("\nNinja Registration:\n1. Register Ninjas" +
+                    "\n2. Displays list ninjas\n3. Exit\nChoose an option: ");
+            int choose = getIntInput(input);
+
+            switch (choose) {
+                case 1:
+                    if (ninjasRegistered < 5) {
+                        System.out.print("Enter the name of the ninja to be registered: ");
+                        String ninjaNameRegister = input.next();
+                        ninjas[ninjasRegistered] = ninjaNameRegister;
+                        ninjasRegistered++;
+                        System.out.println(ninjaNameRegister + " has been registered!");
+                    } else {
+                        System.out.println("\nThe ninja list is full!");
+                    }
+                    break;
+
+                case 2:
+                    if (ninjasRegistered == 0) {
+                        System.out.println("\nThe list is empty.");
+                    } else {
+                        System.out.println("\nList of ninjas registered:");
+                        for (int i = 0; i < ninjasRegistered; i++) {
+                            System.out.println(ninjas[i]);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting the program...");
+                    input.close();
+                    return;
+
+                default:
+                    System.out.println("\nInvalid option!");
+            }
+        }
+    }
+
+    public static int getIntInput(Scanner input) {
+        while (true) {
+            try {
+                return input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Only integer numbers are allowed!");
+                input.nextLine();
+            }
+        }
+    }
+}
+
+
---
+++ JavaNinja/src/Conditions/challenge02/README.md
@@ -0,0 +1,8 @@
+You have been assigned by the Hokage to manage the ninja registration in the Hidden Leaf Village.
+Your mission is to create a simple system in Java to register new ninjas and list all registered ninjas.
+
+Topics to be used:
+- Arrays: To store the names of the ninjas
+- Switch Cases: To implement an interactive menu
+- Loops: To navigate through menu options and iterate over registered ninjas
+- Conditionals: To control the execution of menu options and validate the number of registered ninjas
